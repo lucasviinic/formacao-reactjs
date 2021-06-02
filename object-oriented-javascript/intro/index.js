c6681f7ad0048afe4433753f34fe219a234393ed
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-06-02 19:06:25 -0300

    Create index.js

---
+++ object-oriented-javascript/intro/index.js
@@ -0,0 +1,44 @@
+class Cliente {
+    nome;
+    cpf;
+}
+
+class ContaCorrente {
+    agencia;
+    //#saldo, uma proposta para tornar atributos privados
+    _saldo;
+
+    saca(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor
+            return valor;
+        }
+    }
+
+    deposita(valor) {
+        if (valor < 0) return;
+        this._saldo += valor
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Carlos"
+cliente1.cpf = 83422392392
+cliente1.agencia = 1001
+cliente1.saldo = 0
+
+const cliente2 = new Cliente();
+cliente2.nome = "Ana"
+cliente2.cpf = 88822392392
+cliente2.agencia = 1001
+cliente2.saldo = 10
+
+const contaCorrenteCarlos = new ContaCorrente()
+contaCorrenteCarlos.agencia = 101
+contaCorrenteCarlos._saldo = 1000
+contaCorrenteCarlos.deposita(50)
+contaCorrenteCarlos.saca(100)
+
+
+console.log(contaCorrenteCarlos)
+
