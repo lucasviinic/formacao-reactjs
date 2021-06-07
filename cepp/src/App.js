1f2235487441ad4f8c3a60be152b15d11a6d75e1
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-06-07 20:14:19 -0300

    Estilizando componentes

--- cepp/src/App.js
+++ cepp/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import ListaDeNotas from './components/ListaDeNotas'
-import FormularioCadastro from './components/FormularioCadastro'
+import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas'
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro'
+import "./App.css";
 
 class App extends Component {
   render() {
