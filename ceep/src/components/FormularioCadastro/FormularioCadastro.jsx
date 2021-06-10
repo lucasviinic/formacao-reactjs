f25b456e84dd1e4aaf5f95e7fc92185dfe5c1d81
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-06-09 21:07:05 -0300

    Controlando o estado e a renderização
    - Pensando sobre como transmitir informações entre objetos;
    - Fluxo de atualização do sistema;
    - Para que serve o state;
    - Como atribuir o state de maneira segura;
    - Atualizando a visualização da tela dinamicamente

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,29 +2,40 @@ import React, { Component } from "react";
 import "./style.css";
 class FormularioCadastro extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.titulo = "";
+    this.nota = "";
   }
 
-  handlerMudancaTitulo(evento){
+  _handlerMudancaTitulo(evento){
     this.titulo = evento.target.value
-    console.log(this.titulo)
+  }
+
+  _handlerMudancaTexto(evento){
+    this.texto = evento.target.value
+  }
+
+  _criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
   }
 
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handlerMudancaTitulo.bind(this)}
+          onChange={this._handlerMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handlerMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -3,13 +3,14 @@ import CardNota from '../CardNota';
 import './style.css';
 
 class ListaDeNotas extends Component {
+
   render() {
     return (
       <ul className='lista-notas'>
-      {Array.of('Trabalho', 'Trabalho', 'Estudos', 1, 1, 1, 1).map((categoria, index) => {
+      {this.props.notas.map((nota, index) => {
           return (
             <li className='lista-notas_item' key={index}>
-              <CardNota />
+              <CardNota titulo={nota.titulo} texto={nota.texto}/>
             </li>
           )
         })}
