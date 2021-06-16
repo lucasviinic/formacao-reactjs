5bcaf4710e10e6b27603945192d4932a9e902703
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-06-15 21:00:04 -0300

    Começando a aplicar o conceito de Observable

--- component-life-cycle/ceep/src/App.js
+++ component-life-cycle/ceep/src/App.js
@@ -4,48 +4,24 @@ import FormularioCadastro from './components/FormularioCadastro'
 import ListaDeCategorias from './components/ListaDeCategorias'
 import './assets/App.css'
 import './assets/index.css'
+import Categorias from './dados/Categorias'
+import Notas from './dados/Notas'
 
 class App extends Component {
 
   constructor(){
     super();
-    this.state = {
-      notas: [],
-      categorias: [],
-    }
+    this.categorias = new Categorias();
+    this.notas = new Notas();
   }
-
-  criarNota(titulo, texto, categoria){
-    const novaNota = {titulo, texto, categoria};
-    const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
-
-  deletarNota(index){
-    let arrayNotas = this.state.notas
-    arrayNotas.splice(index, 1)
-    this.setState({notas: arrayNotas})
-  }
-
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
-    const novoEstado = {
-      categorias:novoArrayCategorias
-    }
-    this.setState(novoEstado)
-  }
-
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)} categorias={this.state.categorias}/>
+        <FormularioCadastro criarNota={this.notas.criarNota} categorias={this.categorias.categorias}/>
         <main className="conteudo-principal">
-          <ListaDeCategorias categorias={this.state.categorias} adicionarCategoria={this.adicionarCategoria.bind(this)}/>
-          <ListaDeNotas notas={this.state.notas} apagarNota={this.deletarNota.bind(this)}/>
+          <ListaDeCategorias categorias={this.categorias.categorias} adicionarCategoria={this.categorias.adicionarCategoria}/>
+          <ListaDeNotas notas={this.notas.notas} apagarNota={this.notas.apagarNota}/>
         </main> 
       </section>
     );
