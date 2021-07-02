d575fa1b3862356c3e188629c2b1f2768db757bf
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-01 22:10:06 -0300

    Exibindo os posts

--- react-router-navigation-in-a-spa/petshop/src/components/ListaPost.jsx
+++ react-router-navigation-in-a-spa/petshop/src/components/ListaPost.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { busca } from '../api/api'
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
+import { busca } from "../api/api";
 
-const ListaPost = () => {
-    return (
-        <section className="posts container">
+const ListaPost = ({ url }) => {
+  const [posts, setPosts] = useState([]);
 
-        </section>
-    )
-}
+  useEffect(() => {
+    busca(url, setPosts)
+  },[])
 
-export default ListaPost
+  return (
+    <section className="posts container">
+      {posts.map((post) => (
+        <Link className={`cartao-post cartao-post--${post.categoria}`}>
+            <article key={post.id}>
+                <h3 className="cartao-post__titulo">{post.title}</h3>
+                <p className="cartao-post__meta">{post.metadescription}</p>
+            </article>
+        </Link>
+      ))}
+    </section>
+  );
+};
+
+export default ListaPost;
--- react-router-navigation-in-a-spa/petshop/src/paginas/Home.jsx
+++ react-router-navigation-in-a-spa/petshop/src/paginas/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import ListaPost from '../components/ListaPost'
 
 const Home = () => {
-
   return (
     <main>
       <div className="container">
         <h2 className="titulo-pagina">Pet notícias</h2>
       </div>
+      <ListaPost url={'/posts'} />
     </main>
   )
 }
