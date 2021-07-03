cc7e2e6f4844867dae6960cacfd2c83d77bc8151
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-02 21:11:09 -0300

    Exibindo post e categoria

--- react-router-navigation-in-a-spa/petshop/src/App.jsx
+++ react-router-navigation-in-a-spa/petshop/src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
 import PageNotFound from "./paginas/PageNotFound";
 import Cabecalho from './components/Cabecalho'
+import Post from './paginas/Post'
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route path="/posts/:id">
+          <Post />
         </Route>
         <Route>
           <PageNotFound />
