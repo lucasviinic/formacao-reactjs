d7a9b51f091d821fc474deb8ae2ea00e0b0bd242
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-05 18:40:00 -0300

    Adicionando subcategorias

--- react-router-navigation-in-a-spa/petshop/src/App.jsx
+++ react-router-navigation-in-a-spa/petshop/src/App.jsx
@@ -8,6 +8,7 @@ import Sobre from "./paginas/Sobre";
 import PageNotFound from "./paginas/PageNotFound";
 import Cabecalho from './components/Cabecalho'
 import Post from './paginas/Post'
+import Categoria from './paginas/Categoria'
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route path="/categoria/:id">
+          <Categoria />
         </Route>
         <Route path="/posts/:id">
           <Post />
