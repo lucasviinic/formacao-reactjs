120b01a56b8dfdbd26784cd57bb8da0a2a0c7278
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-07-08 17:04:25 -0300

    Organizando o arquivo de teste

--- automatizacao-de-testes/src/App.test.js
+++ automatizacao-de-testes/src/App.test.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-import App from './app'
+import App from "./app";
 
 describe("Componente principal", () => {
-  it("Mostrar o nome do banco", () => {
-    render(<App />)
+  describe("Quando eu abro o app do banco", () => {
+    it("O nome é exibido", () => {
+      render(<App />);
 
-    expect(screen.getByText('ByteBank')).toBeInTheDocument()
-  })
-})
+      expect(screen.getByText("ByteBank")).toBeInTheDocument();
+    });
+
+    it("O saldo é exibido", () => {
+      render(<App />);
+
+      expect(screen.getByText("Saldo:")).toBeInTheDocument();
+    });
+
+    it("O botão de realizar transação é exibido", () => {
+      render(<App />);
+
+      expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
+  });
+});
